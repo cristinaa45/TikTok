e63ccbdab307d23ae2ce428e1c50bb22a5ca29d1
cristinaa45 <cristinavalarezo6@gmail.com>
2025-04-09 13:14:50 -0400

    Update java.js

--- java.js
+++ java.js
@@ -1,1 +1,12 @@
+const Div = document.getElementById('MCDonald');
+const Div = document.getElementById('Trump');
 
+const infoDiv = document.getElementById('info');
+
+MCDonaldDiv.addEventListener('click', function() {
+  infoDiv.innerHTML = 'This Is MCDONALD';
+});
+
+TrumpDiv.addEventListener('click', function() {
+  infoDiv.innerHTML = 'Trump Working on MCDONALD? WHAAAAAA';
+});

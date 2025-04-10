522f4be448e9069a731e3730461c44c5c3ddd23c
cristinaa45 <cristinavalarezo6@gmail.com>
2025-04-10 13:20:05 -0400

    Update java.js

--- java.js
+++ java.js
@@ -10,3 +10,20 @@ TikTokDiv.addEventListener('click', function() {
 FrederickDiv.addEventListener('click', function() {
   infoDiv.innerHTML = 'Frederick Douglass bio';
 });
+
+document.getElementById("checkButton").addEventListener("click", function() {
+  let age = document.getElementById("").value;
+    let resultText = document.getElementById("");
+  let resultImage = document.getElementById("");
+
+if (age > 16) {
+resultText.innerText = "You cannot drive";
+  resultText.style.color = "red"; // change color
+  resultImage.src = "https://upload.wikimedia.org/wikipedia/commons/4/41/Left_side_of_Flying_Pigeon.jpg";
+} else if (age 16 && age 24) {
+resultText.innerText = "You can drive";
+  resultText.style.color = "green";
+  resultImage.src = "https://car-images.bauersecure.com/wp-images/2697/best-electric-cars-2025-renault-scenic-white-front-driving.jpg";
+} else if (age 25) {
+resultText.innerText = "You can drive and rent a car";
+  resultText.style.color

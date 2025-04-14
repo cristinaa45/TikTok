757a2d12be2c646fcb780c08784dd975676eb52a
cristinaa45 <cristinavalarezo6@gmail.com>
2025-04-14 13:16:00 -0400

    Update java.js

--- java.js
+++ java.js
@@ -16,15 +16,15 @@ document.getElementById("checkButton").addEventListener("click", function() {
     let resultText = document.getElementById("");
   let resultImage = document.getElementById("");
 
-if (age > 16) {
+if (age < 16) {
 resultText.innerText = "You cannot drive";
   resultText.style.color = "red"; // change color
   resultImage.src = "https://upload.wikimedia.org/wikipedia/commons/4/41/Left_side_of_Flying_Pigeon.jpg";
-} else if (age 16 && age 24) {
+} else if (age >= 16 && age <= 24) {
 resultText.innerText = "You can drive";
   resultText.style.color = "green";
   resultImage.src = "https://car-images.bauersecure.com/wp-images/2697/best-electric-cars-2025-renault-scenic-white-front-driving.jpg";
-} else if (age 25) {
+} else if (age > 25) {
 resultText.innerText = "You can drive and rent a car";
   resultText.style.color
 

15e7160be373514efa0a92a4cbff06325d1420f5
cristinaa45 <cristinavalarezo6@gmail.com>
2025-04-14 13:00:07 -0400

    Update java.js

--- java.js
+++ java.js
@@ -47,7 +47,7 @@ for (let i = 1; i <= 20; i++) {
   const p = document.createElement("p");
   p.textContent = i;
 
-  if (i % 2 !==0) {
+  if (i % 2 ==0) {
     p.style.color = "blue";
   } else {
     p.style.color = "red";
